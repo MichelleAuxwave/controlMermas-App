3c171ed6b931ee6fb930bc23692b7f4487366bb7
MichelleAuxwave <michelle@auxwave.mx>
2018-05-16 17:36:22 -0500

    Miercoles 16 de mayo a las 05:36pm

--- src/pages/grupoabierto/grupoabierto.ts
+++ src/pages/grupoabierto/grupoabierto.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the GrupoabiertoPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-grupoabierto',
@@ -15,11 +8,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class GrupoabiertoPage {
 
+  radioColor: string = 'dark';
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad GrupoabiertoPage');
+  }
+
+  cambiarRadioColor(){
+    this.radioColor = 'shamir1';
   }
 
 }
