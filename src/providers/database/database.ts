cf79e42e16cdfd896b77299b9e52625875d8edda
MichelleAuxwave <michelle@auxwave.mx>
2018-05-22 17:33:42 -0500

    Martes 22 de Mayo a las 05:33pm

--- src/providers/database/database.ts
+++ src/providers/database/database.ts
@@ -9,11 +9,11 @@ export class DatabaseProvider {
   private db : SQLiteObject;
   private isOpen : boolean;
 
-  constructor(
-    public http: Http,
-    public storage: SQLite,
-    public toastCtrl: ToastController
-  ) {
+  constructor(public http: Http, public storage: SQLite, public toastCtrl: ToastController) {
+    this.abrirDB();
+  }
+
+  abrirDB(){
     if(!this.isOpen){
       this.storage = new SQLite();
       this.storage.create({name: "data.db", location: "default"}).then((db:SQLiteObject) => {
@@ -34,44 +34,54 @@ export class DatabaseProvider {
   }
 
   guardarOrden(ord: number, tip: string, obs: string){
-    return new Promise((resolve, reject) =>{
-      let sql = "insert into mermasguardadas (ord, tip, obs) values (?, ?, ?)";
-      this.db.executeSql(sql, [ord, tip, obs]).then((data) => {
-        resolve(data);
-      }, (error) => {
-        reject(error);
-        let eee = this.toastCtrl.create({
-          message: error,
-          duration: 5000
+    if(this.isOpen){
+      return new Promise((resolve, reject) =>{
+        let sql = "insert into mermasguardadas (ord, tip, obs) values (?, ?, ?)";
+        this.db.executeSql(sql, [ord, tip, obs]).then((data) => {
+          resolve(data);
+        }, (error) => {
+          reject(error);
+          let eee = this.toastCtrl.create({
+            message: error,
+            duration: 5000
+          });
+          eee.present();
         });
-        eee.present();
       });
-    });
+    }
+    else{
+      this.abrirDB();
+    }
   }
 
   mostrarOrdenesGuardadas(){
-    return new Promise ((resolve, reject) => {
-      this.db.executeSql("select * from mermasguardadas", []).then((data) => {
-        let arrayOrdenes = [];
-        if(data.rows.lenght > 0){
-          for(var i = 0; i < data.rows.lenght; i++){
-             arrayOrdenes.push({
-              ord: data.rows.item(i).ord,
-              tip: data.rows.item(i).tip,
-              obs: data.rows.item(i).obs
-            });
+    if(this.isOpen){
+      return new Promise ((resolve, reject) => {
+        this.db.executeSql("select * from mermasguardadas", []).then((data) => {
+          let arrayOrdenes = [];
+          if(data.rows.lenght > 0){
+            for(var i = 0; i < data.rows.lenght; i++){
+               arrayOrdenes.push({
+                ord: data.rows.item(i).ord,
+                tip: data.rows.item(i).tip,
+                obs: data.rows.item(i).obs
+              });
+            }
           }
-        }
-        resolve(arrayOrdenes);
-      }, (error) =>{
-        reject(error);
-        let eee = this.toastCtrl.create({
-          message: error,
-          duration: 5000
-        });
-        eee.present();
+          resolve(arrayOrdenes);
+        }, (error) =>{
+          reject(error);
+          let eee = this.toastCtrl.create({
+            message: error,
+            duration: 5000
+          });
+          eee.present();
+        })
       })
-    })
+    }
+    else{
+      this.abrirDB();
+    }
   }
 
 }
